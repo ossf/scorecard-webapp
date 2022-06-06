35a48f01ae6b77b4c482888acac32cf7afb0bea6
Azeem Shaikh <azeemshaikh38@gmail.com>
2022-06-06 12:55:26 -0700

    Update the POST API request (#111)
    Co-authored-by: Azeem Shaikh <azeems@google.com>

--- app/post_results.go
+++ app/post_results.go
@@ -30,9 +30,11 @@ import (
 
 	"github.com/go-openapi/runtime"
 	"github.com/google/go-github/v42/github"
+	"github.com/gorilla/mux"
 	"github.com/sigstore/cosign/cmd/cosign/cli/options"
 	"github.com/sigstore/cosign/cmd/cosign/cli/rekor"
 	"github.com/sigstore/cosign/pkg/cosign"
+	"github.com/sigstore/rekor/pkg/generated/client"
 	"github.com/sigstore/rekor/pkg/generated/models"
 	"github.com/sigstore/rekor/pkg/types"
 	hashedrekord "github.com/sigstore/rekor/pkg/types/hashedrekord/v0.0.1"
@@ -41,91 +43,143 @@ import (
 	"gocloud.dev/blob"
 )
 
-type ScorecardOutput struct {
-	JSONOutput string
+const (
+	// OID source: https://github.com/sigstore/fulcio/blob/96ef49cc7662912ba37d46f738757e8d8d5b5355/docs/oid-info.md#L33
+	// TODO: retrieve these by name.
+	fulcioRepoRefKey  = "1.3.6.1.4.1.57264.1.6"
+	fulcioRepoPathKey = "1.3.6.1.4.1.57264.1.5"
+	fulcioRepoSHAKey  = "1.3.6.1.4.1.57264.1.3"
+	resultsBucket     = "gs://ossf-scorecard-results"
+	resultsFile       = "results.json"
+)
+
+var (
+	errWritingBucket            = errors.New("error writing to GCS bucket")
+	errMultipleCerts            = errors.New("multiple certificates found for the entry")
+	errEmptyCertRef             = errors.New("cert has empty repository ref")
+	errEmptyCertPath            = errors.New("cert has empty repository path")
+	errCertMissingURI           = errors.New("certificate has no URIs")
+	errCertWorkflowPathEmpty    = errors.New("cert workflow path is empty")
+	errMismatchedCertAndRequest = errors.New("repository and branch of cert doesn't match that of request")
+)
+
+type ScorecardResult struct {
+	Result      string `json:"result"`
+	Branch      string `json:"branch"`
+	AccessToken string `json:"accessToken"`
+}
+
+type certInfo struct {
+	repoFullName  string
+	repoBranchRef string
+	repoSHA       string
+	workflowPath  string
 }
 
 func PostResultsHandler(w http.ResponseWriter, r *http.Request) {
-	err := handler(w, r)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Println(err)
-		return
-	}
-}
+	// Sanity check
+	host := mux.Vars(r)["host"]
+	orgName := mux.Vars(r)["orgName"]
+	repoName := mux.Vars(r)["repoName"]
 
-func handler(w http.ResponseWriter, r *http.Request) error {
-	ctx := context.Background()
 	reqBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		return fmt.Errorf("error reading http request body: %v", err)
+		w.WriteHeader(http.StatusBadRequest)
+		_, err := fmt.Fprintf(w, "error reading request body")
+		if err != nil {
+			log.Printf("error during Write: %v", err)
+		}
+		return
+	}
+	var scorecardResult ScorecardResult
+	if err := json.Unmarshal(reqBody, &scorecardResult); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		_, err := fmt.Fprint(w, "error unmarshaling input JSON")
+		if err != nil {
+			log.Printf("error during Write: %v", err)
+		}
+		return
 	}
 
-	// Unmarshal request body.
-	var scorecardOutput ScorecardOutput
-	err = json.Unmarshal(reqBody, &scorecardOutput)
+	// Process
+	err = processRequest(host, orgName, repoName, scorecardResult)
+	if err == nil {
+		w.WriteHeader(http.StatusCreated)
+		return
+	}
+	if errors.Is(err, errMismatchedCertAndRequest) {
+		w.WriteHeader(http.StatusBadRequest)
+		if _, errWrite := fmt.Fprintf(w, "%v", err); errWrite != nil {
+			log.Printf("%v", errWrite)
+		}
+		return
+	}
+	log.Printf("%v", err)
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+func processRequest(host, org, repo string, scorecardResult ScorecardResult) error {
+	rekorClient, err := rekor.NewClient(options.DefaultRekorURL)
 	if err != nil {
-		return fmt.Errorf("error unmarshalling request body: %v", err)
+		return fmt.Errorf("error initializing Rekor client: %w", err)
 	}
 
-	// Grab parameters.
-	reqRepo := r.Header["X-Repository"]
-	reqBranch := r.Header["X-Branch"]
-	if len(reqRepo) == 0 || len(reqBranch) == 0 {
-		return fmt.Errorf("error: empty parameters")
-	}
-
-	err = verifySignature(ctx, scorecardOutput, reqRepo[0], reqBranch[0])
+	ctx := context.Background()
+	cert, err := extractAndVerifyCertForPayload(ctx, rekorClient, []byte(scorecardResult.Result))
 	if err != nil {
-		return err
+		return fmt.Errorf("error extracting cert: %w", err)
 	}
 
-	// Write response.
-	w.Write([]byte(
-		fmt.Sprintf(
-			"Successfully verified and uploaded scorecard results for repo %s on branch %s",
-			reqRepo[0], reqBranch[0])))
+	info, err := extractCertInfo(cert)
+	if err != nil {
+		return fmt.Errorf("error extracting cert info: %w", err)
+	}
+	if info.repoFullName != fmt.Sprintf("%s/%s", org, repo) ||
+		(info.repoBranchRef != scorecardResult.Branch &&
+			info.repoBranchRef != fmt.Sprintf("refs/heads/%s", scorecardResult.Branch)) {
+		return fmt.Errorf("%w", errMismatchedCertAndRequest)
+	}
 
+	if err := getAndVerifyWorkflowContent(ctx, org, repo, scorecardResult, info); err != nil {
+		return fmt.Errorf("error verifying workflow: %w", err)
+	}
+
+	// Save scorecard results (results.json, score.txt) to GCS
+	bucketURL := resultsBucket
+	objectPath := fmt.Sprintf("%s/%s/%s/%s", host, org, repo, resultsFile)
+
+	if err := writeToBlobStore(ctx, bucketURL, objectPath, []byte(scorecardResult.Result)); err != nil {
+		return fmt.Errorf(errWritingBucket.Error()+": %v, %v", err)
+	}
 	return nil
 }
 
-var errorWritingBucket = errors.New("error writing to GCS bucket")
-
-// verifySignature receives the scorecard analysis payload, looks up its associated tlog entry and
-// certificate, and extracts the repository's workflow file to ensure its legitimacy.
-func verifySignature(ctx context.Context, scorecardOutput ScorecardOutput, reqRepo, reqBranch string) error {
-	// Lookup results payload to get the repo info from the corresponding entry & cert.
-	repoPath, repoRef, repoSHA, workflowPath, err := lookupPayload(ctx, []byte(scorecardOutput.JSONOutput))
-	if err != nil {
-		return fmt.Errorf("error looking up json results: %v", err)
-	}
-
-	// Split repo path into owner and repo name.
-	ownerName := repoPath[:strings.Index(repoPath, "/")]
-	repoName := repoPath[strings.Index(repoPath, "/")+1:]
-
-	// Verify that the repository and branch of the cert and request are equal.
-	if len(reqRepo) == 0 || len(reqBranch) == 0 || reqRepo != repoPath || reqBranch != repoRef {
-		return fmt.Errorf("repository and branch of cert doesn't match that of request")
-	}
-
+func getAndVerifyWorkflowContent(ctx context.Context,
+	org, repo string, scorecardResult ScorecardResult, info certInfo,
+) error {
 	// Get the corresponding GitHub repository.
-	// TODO: use GITHUB_TOKEN from workflow to make the api call.
-	client := github.NewClient(nil)
-	repo, _, err := client.Repositories.Get(ctx, ownerName, repoName)
+	httpClient := http.DefaultClient
+	if scorecardResult.AccessToken != "" {
+		httpClient.Transport = githubTransport{
+			token: scorecardResult.AccessToken,
+		}
+	}
+	client := github.NewClient(httpClient)
+	repoClient, _, err := client.Repositories.Get(ctx, org, repo)
 	if err != nil {
-		return fmt.Errorf("error getting repository: %v", err)
+		return fmt.Errorf("error getting repository: %w", err)
 	}
 
 	// Verify that the branch from the results files is the repo's default branch.
-	defaultBranch := "refs/heads/" + repo.GetDefaultBranch()
-	if defaultBranch != repoRef {
+	defaultBranch := repoClient.GetDefaultBranch()
+	if scorecardResult.Branch != defaultBranch &&
+		scorecardResult.Branch != fmt.Sprintf("refs/heads/%s", defaultBranch) {
 		return fmt.Errorf("branch of cert isn't the repo's default branch")
 	}
 
 	// Get workflow file from cert commit SHA.
-	opts := &github.RepositoryContentGetOptions{Ref: repoSHA}
-	contents, _, _, err := client.Repositories.GetContents(ctx, ownerName, repoName, workflowPath, opts)
+	opts := &github.RepositoryContentGetOptions{Ref: info.repoSHA}
+	contents, _, _, err := client.Repositories.GetContents(ctx, org, repo, info.workflowPath, opts)
 	if err != nil {
 		return fmt.Errorf("error downloading workflow contents from repo: %v", err)
 	}
@@ -136,20 +190,8 @@ func verifySignature(ctx context.Context, scorecardOutput ScorecardOutput, reqRe
 	}
 
 	// Verify scorecard workflow.
-	err = verifyScorecardWorkflow(workflowContent)
-	if err != nil {
+	if err := verifyScorecardWorkflow(workflowContent); err != nil {
 		return fmt.Errorf("workflow could not be verified: %v", err)
-	}
-
-	// Save scorecard results (results.json, score.txt) to GCS
-	bucketURL := "gs://ossf-scorecard-results"
-	folderPath := fmt.Sprintf("%s/%s", "github.com", repoPath)
-	jsonPath := fmt.Sprintf("%s/results.json", folderPath)
-
-	err = writeToBlobStore(ctx, bucketURL, jsonPath, []byte(scorecardOutput.JSONOutput))
-
-	if err != nil {
-		return fmt.Errorf(errorWritingBucket.Error()+": %v, %v", err)
 	}
 	return nil
 }
@@ -174,84 +216,82 @@ func writeToBlobStore(ctx context.Context, bucketURL, filename string, data []by
 	return nil
 }
 
-func lookupPayload(ctx context.Context, payload []byte) (repoPath, repoRef, repoSHA, workflowPath string, err error) {
+func extractAndVerifyCertForPayload(ctx context.Context,
+	rekorClient *client.Rekor, payload []byte,
+) (*x509.Certificate, error) {
 	// Get most recent Rekor entry uuid.
-	rekorClient, err := rekor.NewClient(options.DefaultRekorURL)
-	if err != nil {
-		return "", "", "", "", fmt.Errorf("error initializing Rekor client: %v", err)
-	}
-
 	uuids, err := cosign.FindTLogEntriesByPayload(ctx, rekorClient, payload)
 	if err != nil || len(uuids) == 0 {
-		return "", "", "", "", fmt.Errorf("error finding tlog entries corresponding to payload: %v", err)
+		return nil, fmt.Errorf("error finding tlog entries corresponding to payload: %w", err)
 	}
 	uuid := uuids[len(uuids)-1] // ignore past entries.
 
 	// Get tlog entry from the UUID.
 	entry, err := cosign.GetTlogEntry(ctx, rekorClient, uuid)
 	if err != nil {
-		return "", "", "", "", fmt.Errorf("error fetching tlog entry: %v", err)
+		return nil, fmt.Errorf("error fetching tlog entry: %w", err)
 	}
 
 	// Verify tlog entry to make sure it is actually in the log.
 	err = cosign.VerifyTLogEntry(ctx, rekorClient, entry)
 	if err != nil {
-		return "", "", "", "", fmt.Errorf("error verifying tlog entry: %v", err)
+		return nil, fmt.Errorf("error verifying tlog entry: %w", err)
 	}
 
 	// Extract certificate.
 	certs, err := extractCerts(entry)
 	if err != nil || len(certs) == 0 {
-		return "", "", "", "", fmt.Errorf("error extracting certificate from entry: %v", err)
+		return nil, fmt.Errorf("error extracting certificate from entry: %w", err)
 	}
 	if len(certs) > 1 {
-		return "", "", "", "", fmt.Errorf("multiple certificates found for the entry: %v", err)
+		return nil, fmt.Errorf("%w", errMultipleCerts)
 	}
 
 	cert := certs[0]
-
 	// Verify that cert isn't expired.
 	if err = cosign.CheckExpiry(cert, time.Unix(*entry.IntegratedTime, 0)); err != nil {
-		return "", "", "", "", fmt.Errorf("cosign certificate is expired: %v", err)
+		return nil, fmt.Errorf("error during cosign.CheckExpiry: %w", err)
 	}
+	return cert, nil
+}
 
+func extractCertInfo(cert *x509.Certificate) (certInfo, error) {
+	ret := certInfo{}
 	// Get repo reference & path from cert.
 	for _, ext := range cert.Extensions {
-		// OID source: https://github.com/sigstore/fulcio/blob/96ef49cc7662912ba37d46f738757e8d8d5b5355/docs/oid-info.md#L33
-		// TODO: retrieve these by name.
-		if ext.Id.String() == "1.3.6.1.4.1.57264.1.6" {
-			repoRef = string(ext.Value)
+		if ext.Id.String() == fulcioRepoRefKey {
+			if len(ext.Value) == 0 {
+				return ret, fmt.Errorf("%w", errEmptyCertRef)
+			}
+			ret.repoBranchRef = string(ext.Value)
 		}
-		if ext.Id.String() == "1.3.6.1.4.1.57264.1.5" {
-			repoPath = string(ext.Value)
+		if ext.Id.String() == fulcioRepoPathKey {
+			if len(ext.Value) == 0 {
+				return ret, fmt.Errorf("%w", errEmptyCertPath)
+			}
+			ret.repoFullName = string(ext.Value)
 		}
-		if ext.Id.String() == "1.3.6.1.4.1.57264.1.3" {
-			repoSHA = string(ext.Value)
+		if ext.Id.String() == fulcioRepoSHAKey {
+			ret.repoSHA = string(ext.Value)
 		}
-	}
-
-	if len(repoRef) == 0 || len(repoPath) == 0 {
-		return "", "", "", "", fmt.Errorf("error extracting repo ref or path from certificate %v", err)
 	}
 
 	// Get workflow job ref from the certificate.
-	certURIs := cert.URIs
-	if len(certURIs) == 0 {
-		return "", "", "", "", errors.New("error: certificate has no URIs")
+	if len(cert.URIs) == 0 {
+		return ret, errCertMissingURI
 	}
-	workflowRef := certURIs[0].Path
+	workflowRef := cert.URIs[0].Path
 	if len(workflowRef) == 0 {
-		return "", "", "", "", errors.New("error: workflow path is empty")
+		return ret, errCertWorkflowPathEmpty
 	}
 
 	// Remove repo path from workflow filepath.
-	ind := strings.Index(workflowRef, repoPath) + len(repoPath) + 1
-	workflowPath = workflowRef[ind:]
+	ind := strings.Index(workflowRef, ret.repoFullName) + len(ret.repoFullName) + 1
+	ret.workflowPath = workflowRef[ind:]
 
 	// Remove repo ref tag from workflow filepath.
-	workflowPath = workflowPath[:strings.Index(workflowPath, "@")]
-
-	return repoPath, repoRef, repoSHA, workflowPath, nil
+	ret.workflowPath = ret.workflowPath[:strings.Index(ret.workflowPath, "@")]
+	return ret, nil
 }
 
 // nolint:lll

71738b3c7db553352763b655a6e20d0cd24492d8
Azeem Shaikh <azeemshaikh38@gmail.com>
2022-08-11 20:24:24 -0400

    Add Netlify badge to the site (#165)

--- scorecards-site/dist/_nuxt/static/1649863735/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649863735",layout:"default",error:a,state:{bgColour:a,"mutation-types":{},settings:{bgColour:a,textColour:a}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"74eeab55"}}}}(null,"\u002F"));

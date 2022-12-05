b7e0f8dea5ddf9bd9d59c81a3c5b3f924842ec0c
Arnaud J Le Hors <lehors@us.ibm.com>
2022-12-05 15:05:39 +0100

    Change project name to OpenSSF Scorecard on website and in copyright (#271)
    Also added a section to the homepage to explain the project name change.
    
    Notes: change wasn't made inside quotes but a note was inserted to add
    a reference to the new name.
    The SVG logo still needs to be updated.
    
    Signed-off-by: Arnaud J Le Hors <lehors@us.ibm.com>
    
    Signed-off-by: Arnaud J Le Hors <lehors@us.ibm.com>

--- app/generated/restapi/configure_scorecard.go
+++ app/generated/restapi/configure_scorecard.go
@@ -1,6 +1,6 @@
 // This file is safe to edit. Once it exists it will not be overwritten
 
-// Copyright 2021 Security Scorecard Authors
+// Copyright 2021 OpenSSF Scorecard Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.

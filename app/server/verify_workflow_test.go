c57f630ec7240dfb4a3e213d1eafd7ea8dc8a117
Spencer Schrock <sschrock@google.com>
2023-04-24 15:08:25 -0700

    Add tests for imposter commits
    Signed-off-by: Spencer Schrock <sschrock@google.com>

--- app/server/verify_workflow_test.go
+++ app/server/verify_workflow_test.go
@@ -22,17 +22,36 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type allowListVerifier struct {
+	allowed map[string]bool
+}
+
+func (a *allowListVerifier) contains(owner, repo, base, target string) (bool, error) {
+	return a.allowed[target], nil
+}
+
+var allowCommitVerifier = &allowListVerifier{
+	allowed: map[string]bool{
+		"dd2c410b088af7c0dc8046f3ac9a8f4148492a95": true,
+		"ec3a7ce113134d7a93b817d10a8272cb61118579": true,
+		"c8416b0b2bf627c349ca92fc8e3de51a64b005cf": true,
+		"82c141cc518b40d92cc801eee768e7aafc9c2fa2": true,
+		"5f532563584d71fdef14ee64d17bafb34f751ce5": true,
+	},
+}
+
 func TestVerifyValidWorkflows(t *testing.T) {
 	t.Parallel()
 	workflowFiles := []string{
 		"testdata/workflow-valid.yml",
 		"testdata/workflow-valid-noglobalperm.yml",
 		"testdata/workflow-valid-e2e.yml",
+		"testdata/workflow-valid-tagged-action.yml",
 	}
 
 	for _, workflowFile := range workflowFiles {
 		workflowContent, _ := os.ReadFile(workflowFile)
-		err := verifyScorecardWorkflow(string(workflowContent), nil)
+		err := verifyScorecardWorkflow(string(workflowContent), allowCommitVerifier)
 		if err != nil {
 			t.Errorf("expected - %v, got - %v", nil, err)
 		}
@@ -60,11 +79,12 @@ func TestVerifyInvalidWorkflows(t *testing.T) {
 		"testdata/workflow-invalid-multiple-labels.yml",
 		"testdata/workflow-invalid-nil-steps.yml",
 		"testdata/workflow-invalid-execaction.yml",
+		"testdata/workflow-invalid-imposter-commit.yml",
 	}
 
 	for _, workflowFile := range workflowFiles {
 		workflowContent, _ := os.ReadFile(workflowFile)
-		err := verifyScorecardWorkflow(string(workflowContent), nil)
+		err := verifyScorecardWorkflow(string(workflowContent), allowCommitVerifier)
 		assert.NotEqual(t, err, nil, workflowFile)
 	}
 }

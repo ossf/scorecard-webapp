693b0e5619c97f0959a54c6ca3109b2be785c03e
afmarcum <138055109+afmarcum@users.noreply.github.com>
2024-04-24 15:12:03 -0500

    Remove link to closed survey (#611)
    * Remove survey link
    
    Signed-off-by: afmarcum <138055109+afmarcum@users.noreply.github.com>
    
    * Add period
    
    Signed-off-by: afmarcum <138055109+afmarcum@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: afmarcum <138055109+afmarcum@users.noreply.github.com>

--- scorecards-site/pages/index.vue
+++ scorecards-site/pages/index.vue
@@ -4,9 +4,6 @@
       class="flex justify-center items-center relative min-h-mobile md:min-h-threeQuarters"
     >
       <div class="mx-auto w-full md:w-4/6 text-center hero-text">
-        <div class="pt-20 pb-32 text-22">
-          Take the <a href="https://forms.gle/aqxZwmVQzWJkNuso8">OpenSSF Scorecard User Survey</a>
-        </div>
         <h1>
           Build better security habits,<br />
           one test at a time
